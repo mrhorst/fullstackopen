560eb1c331d64d87e58fa3539ff3d2f6a165ac8d
mrhorst <mhorst31@gmail.com>
2025-07-29 15:37:16 -0400

    finish all the way to `2.5: separate module step 10`

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,17 +1,24 @@
-const Course = ({ course }) => {
+const Course = ({ courses }) => {
   return (
-    <>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-    </>
+    <ul style={{ paddingLeft: 0 }}>
+      {courses.map((course) => {
+        return (
+          <li style={{ listStyle: 'none' }} key={course.id}>
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total total={course.parts} />
+          </li>
+        )
+      })}
+    </ul>
   )
 }
 
-const Header = (props) => <h1>{props.course}</h1>
+const Header = (props) => <h2>{props.course}</h2>
 
 const Content = (props) => {
   return (
-    <ul>
+    <ul style={{ paddingLeft: 0 }}>
       {props.parts.map((part) => (
         <Part key={part.name} part={part} />
       ))}
@@ -20,10 +27,17 @@ const Content = (props) => {
 }
 
 const Part = (props) => (
-  <li>
-    {props.part.name} {props.part.exercises}
+  <li style={{ listStyle: 'none' }}>
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
   </li>
 )
 
-const Total = (props) => <p>Number of exercises {props.total}</p>
+const Total = (props) => {
+  const total = props.total.reduce((acc, cur) => {
+    return (acc += cur.exercises)
+  }, 0)
+  return <p style={{ fontWeight: 'bold' }}>Number of exercises {total}</p>
+}
 export default Course
