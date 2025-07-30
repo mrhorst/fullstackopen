315564ea7a924aeea7fecc37cf938d1c0b851e62
mrhorst <mhorst31@gmail.com>
2025-07-30 14:40:28 -0400

    phonebook step2 completed.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,41 @@
-function App() {
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [newName, setNewName] = useState('')
+
+  const handleInput = (e) => {
+    e.preventDefault()
+    setNewName(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (isUnique(newName)) {
+      setPersons([...persons, { name: newName }])
+      setNewName('')
+    } else {
+      alert(`${newName} is already present in the phonebook`)
+    }
+  }
+
+  const isUnique = (name) => {
+    return persons.find((p) => p.name !== name)
+  }
+
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input onChange={handleInput} value={newName} />
+        </div>
+        <div>
+          <button>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+    </div>
   )
 }
 
