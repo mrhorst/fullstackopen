5704a12a08a278521ddd58c5db030ef118cb84c6
mrhorst <mhorst31@gmail.com>
2025-07-30 23:29:38 -0400

    finish `phonebook` step5 (2.10)

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -50,28 +50,50 @@ const App = () => {
   return (
     <div>
       <h1>Phonebook</h1>
-      <form>
-        <div>
-          filter shown with{' '}
-          <input name="filter" onChange={handleInput} value={newFilter} />
-        </div>
-      </form>
-      <h2>add a new contact</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          name: <input name="name" onChange={handleInput} value={newName} />
-        </div>
-        <div>
-          number:
-          <input name="phone" onChange={handleInput} value={newNumber} />
-        </div>
-        <div>
-          <button>add</button>
-        </div>
-      </form>
+      <Filter handleInput={handleInput} newFilter={newFilter} />
+      <h3>add a new contact</h3>
+      <PersonForm
+        handleInput={handleInput}
+        handleSubmit={handleSubmit}
+        newName={newName}
+        newNumber={newNumber}
+      />
+
       <h2>Numbers</h2>
       <Persons persons={filteredPersons(newFilter)} />
     </div>
+  )
+}
+
+const PersonForm = ({ handleSubmit, handleInput, newName, newNumber }) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        name: <input name="name" onChange={handleInput} value={newName} />
+      </div>
+      <div>
+        number:
+        <input name="phone" onChange={handleInput} value={newNumber} />
+      </div>
+      <div>
+        <button>add</button>
+      </div>
+    </form>
+  )
+}
+
+const Filter = (props) => {
+  return (
+    <form>
+      <div>
+        filter shown with{' '}
+        <input
+          name="filter"
+          onChange={props.handleInput}
+          value={props.newFilter}
+        />
+      </div>
+    </form>
   )
 }
 
