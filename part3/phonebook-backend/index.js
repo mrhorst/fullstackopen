51164c3b2bd50473004f51bbaa2e0406788f30bf
mrhorst <mhorst31@gmail.com>
2025-08-06 23:07:25 -0400

    [fix] standardize request to be called request and response to be called response, instead of req and res (I was mixing them up)

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -8,48 +8,52 @@ const app = express()
 app.use(express.json())
 app.use(express.static('dist'))
 
-morgan.token('tony', (req, res) => {
-  return JSON.stringify(req.body)
+morgan.token('tony', (request, response) => {
+  return JSON.stringify(request.body)
 })
 
 const tony =
   ':method :url :status :res[content-length] - :response-time ms :tony'
 app.use(morgan(tony))
 
-app.get('/api/persons', (req, res) => {
-  Person.find({}).then((persons) => res.json(persons))
+app.get('/api/persons', (request, respons) => {
+  Person.find({}).then((persons) => respons.json(persons))
 })
 
-app.get('/info', (req, res) => {
+app.get('/info', (request, respons) => {
   const numPersons = entries.length
   const now = new Date().toUTCString()
-  res.send(`<p>Phonebook has info for ${numPersons} people</p><p>${now}</p>`)
+  respons.send(
+    `<p>Phonebook has info for ${numPersons} people</p><p>${now}</p>`
+  )
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = req.params.id
-  Person.findById(request.params.id).then((person) => response.json(person))
+app.get('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  Person.findById(request.params.id)
+    .then((person) => response.json(person))
+    .catch((error) => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  Person.findByIdAndDelete(req.params.id).then((person) => {
+app.delete('/api/persons/:id', (request, respons) => {
+  Person.findByIdAndDelete(request.params.id).then((person) => {
     console.log(`deleted ${person}`)
-    res.status(204).end()
+    response.status(204).end()
   })
 })
 
-app.post('/api/persons', (req, res) => {
-  const { name, number } = req.body
+app.post('/api/persons', (request, response) => {
+  const { name, number } = request.body
 
   if (!name || !number) {
-    return res.status(400).json({
+    return response.status(400).json({
       error: 'content missing',
     })
   }
 
   const person = new Person({ name, number })
 
-  person.save().then((savedPerson) => res.json(savedPerson))
+  person.save().then((savedPerson) => response.json(savedPerson))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -69,7 +73,7 @@ app.put('/api/persons/:id', (request, response, next) => {
     .catch((error) => next(error))
 })
 
-const errorHandler = (error, req, res, next) => {
+const errorHandler = (error, request, response, next) => {
   console.error(`Error: ${error.message}`)
 
   next(error)
