68f56fac823f96323f629f291a20339ec33fb62f
mrhorst <mhorst31@gmail.com>
2025-08-08 23:59:02 -0400

    finish `blog list` step 1 & 2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+
+const app = express()
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
