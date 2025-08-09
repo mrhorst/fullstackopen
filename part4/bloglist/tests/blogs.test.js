3b0391c0a3ecf6e9745891b1e47c35735179232e
mrhorst <mhorst31@gmail.com>
2025-08-09 14:14:32 -0400

    finish `4.4` step 2

--- part4/bloglist/tests/blogs.test.js
+++ part4/bloglist/tests/blogs.test.js
@@ -8,3 +8,55 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
 })
+
+const listWithOneBlog = [
+  {
+    title: 'test with one blog',
+    author: 'Mr. Test',
+    url: 'someurl.com',
+    likes: 2,
+  },
+]
+
+const listWithFiveBlogs = [
+  {
+    title: 'First blog',
+    author: 'Alice',
+    url: 'aliceblog.com',
+    likes: 5,
+  },
+  {
+    title: 'Second blog',
+    author: 'Bob',
+    url: 'bobblog.com',
+    likes: 10,
+  },
+  {
+    title: 'Third blog',
+    author: 'Charlie',
+    url: 'charlieblog.com',
+    likes: 3,
+  },
+  {
+    title: 'Fourth blog',
+    author: 'Dana',
+    url: 'danablog.com',
+    likes: 7,
+  },
+  {
+    title: 'Fifth blog',
+    author: 'Eve',
+    url: 'eveblog.com',
+    likes: 1,
+  },
+]
+
+test('when list has one blog with 2 likes, return 2', () => {
+  const result = listHelper.totalLikes(listWithOneBlog)
+  assert.strictEqual(result, 2)
+})
+
+test('when list has 5 blogs with total of 26 likes, return 26', () => {
+  const result = listHelper.totalLikes(listWithFiveBlogs)
+  assert.strictEqual(result, 26)
+})
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((acc, cur) => {
+    acc += cur.likes
+    return acc
+  }, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
