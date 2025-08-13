c7fcc75e49e07ccf0755317e58e2987e29512a54
mrhorst <mhorst31@gmail.com>
2025-08-13 00:01:10 -0400

    finish `4.17 blog list expansion` step 5

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,9 +1,31 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
+})
+
+blogsRouter.get('/seed', async (request, response) => {
+  const allBlogsStart = await Blog.find({})
+  const users = await User.find({})
+  const randomUser = users[Math.floor(Math.random() * users.length)]
+
+  allBlogsStart.forEach(async (blog) => {
+    if (!blog.user) {
+      blog.user = randomUser._id
+      console.log('Blog to be saved:', blog)
+      await blog.save()
+    } else {
+      const userId = blog.user.toString()
+      const user = await User.findById(userId)
+      user.blogs.push(blog)
+      await user.save()
+    }
+  })
+
+  response.status(200).json({ status: 'OK' })
 })
 
 blogsRouter.post('/', async (request, response) => {
@@ -15,7 +37,16 @@ blogsRouter.post('/', async (request, response) => {
   if (!blog.url) {
     response.status(400).send({ error: 'url is missing' })
   }
+
+  const users = await User.find({})
+  const randomUser = users[Math.floor(Math.random() * users.length)]
+  blog.user = randomUser._id
+
   const result = await blog.save()
+
+  randomUser.notes = randomUser.notes.concat(result._id)
+  await randomUser.save()
+
   response.status(201).json(result)
 })
 
