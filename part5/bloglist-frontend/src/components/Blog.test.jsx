c0ca40fb70ec5bd17507bae15104ab0c74b14642
mrhorst <mhorst31@gmail.com>
2025-08-22 13:41:57 -0400

    finish 5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('blog renders only author and title by default', () => {
@@ -26,4 +27,35 @@ test('blog renders only author and title by default', () => {
   expect(element2).toBeDefined()
   expect(element3).toBeNull()
   expect(element4).toBeNull()
+})
+
+test('url and likes are shown when clicking the button..', async () => {
+  const blog = {
+    id: 1,
+    author: 'Test1 author',
+    title: 'Test1 title',
+    url: 'Test1 url',
+    user: '123',
+  }
+
+  const user = {
+    token: 'dummy',
+    name: 'dummy user',
+    id: '123',
+  }
+
+  const mockHandler = vi.fn()
+
+  render(<Blog blogs={[blog]} user={user} />)
+
+  const user1 = userEvent.setup()
+
+  const button = screen.getByText('show info')
+  await user1.click(button)
+
+  const element = screen.getByText('url', { exact: false })
+  const element2 = screen.getByText('likes', { exact: false })
+
+  expect(element).toBeDefined()
+  expect(element2).toBeDefined()
 })
