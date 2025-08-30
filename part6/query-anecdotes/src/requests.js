1a890738ec56d34a8b331bf38b95dba1cbe489a2
mrhorst <mhorst31@gmail.com>
2025-08-30 14:40:54 -0400

    finish 6.21

--- part6/query-anecdotes/src/requests.js
+++ part6/query-anecdotes/src/requests.js
@@ -2,7 +2,16 @@ import axios from 'axios'
 
 const baseUrl = 'http://localhost:3001/anecdotes'
 
+const getId = () => {
+  return Math.round(Math.random() * 1000000)
+}
+
 export const getAnecdotes = async () => {
   const response = await axios.get(baseUrl)
+  return response.data
+}
+
+export const createAnecdote = async (anecdote) => {
+  const response = await axios.post(baseUrl, { ...anecdote, id: getId() })
   return response.data
 }
