1d22ac2411d27da4ae8e97a30b9e675ee16ae348
mrhorst <mhorst31@gmail.com>
2025-09-04 11:30:41 -0400

    as i always use prettier extension, 7.9 is already completed...

---
+++ part7/bloglist-backend/utils/middleware.js
@@ -0,0 +1,50 @@
+const logger = require('../utils/logger')
+const jwt = require('jsonwebtoken')
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  if (error.name === 'ValidationError') {
+    response.status(400).json({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    response.status(403).json({ error: error.message })
+  } else if (error.name === 'CastError') {
+    response.status(400).json({ error: 'malformatted id' })
+  }
+
+  logger.error('OUR MIDDLEWARE DID NOT CATCH THIS ONE. PLEASE FIX:', error)
+
+  next(error)
+}
+
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  }
+  next()
+}
+
+const userExtractor = (request, response, next) => {
+  try {
+    if (request.token) {
+      const decodedUser = jwt.verify(request.token, process.env.SECRET)
+      if (decodedUser) {
+        request.user = decodedUser
+      }
+    }
+  } catch (e) {
+    next(e)
+  } //
+  next()
+}
+
+module.exports = {
+  unknownEndpoint,
+  errorHandler,
+  tokenExtractor,
+  userExtractor,
+}
