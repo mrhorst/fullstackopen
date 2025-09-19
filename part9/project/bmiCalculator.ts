8598406aca38083447e0ebba0e20b929b8db2c3b
mrhorst <mhorst31@gmail.com>
2025-09-19 13:44:22 -0400

    finish 9.5

--- part9/project/bmiCalculator.ts
+++ part9/project/bmiCalculator.ts
@@ -1,33 +1,42 @@
-const calculateBmi = (height: number, weight: number): void => {
+const calculateBmi = (height: number, weight: number): string => {
   /** height in cm, weight in kg */
   const bmi: number = weight / (height / 100) ** 2
 
+  if (isNaN(bmi)) {
+    throw Error('bmi is not a number')
+  }
+
   if (bmi < 18.5) {
-    console.log('underweight')
-  } else if (bmi >= 18.5 && bmi < 24.9) {
-    console.log('normal')
-  } else if (bmi >= 25 && bmi < 29.9) {
-    console.log('overweight')
+    return 'underweight'
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    return 'normal'
+  } else if (bmi >= 25 && bmi <= 29.9) {
+    return 'overweight'
   } else {
-    console.log('obesity')
+    return 'obesity'
   }
 }
-try {
-  const height: number = Number(process.argv[2])
-  if (isNaN(height)) {
-    throw Error('height is not a number!')
-  }
 
-  const weight: number = Number(process.argv[3])
-  if (isNaN(weight)) {
-    throw Error('weight is not a number!')
-  }
+if (require.main === module) {
+  try {
+    const height: number = Number(process.argv[2])
+    if (isNaN(height)) {
+      throw Error('height is not a number!')
+    }
 
-  calculateBmi(height, weight)
-} catch (error: unknown) {
-  let errorMessage = 'Something is wrong: '
-  if (error instanceof Error) {
-    errorMessage += error.message
+    const weight: number = Number(process.argv[3])
+    if (isNaN(weight)) {
+      throw Error('weight is not a number!')
+    }
+
+    console.log(calculateBmi(height, weight))
+  } catch (error: unknown) {
+    let errorMessage = 'Something is wrong: '
+    if (error instanceof Error) {
+      errorMessage += error.message
+    }
+    console.error(errorMessage)
   }
-  console.error(errorMessage)
 }
+
+export default calculateBmi
--- part9/project/index.ts
+++ part9/project/index.ts
@@ -1,9 +1,31 @@
 import express from 'express'
+import calculateBmi from './bmiCalculator'
 
 const app = express()
 
 app.get('/hello', (_req, res) => {
   res.send('hello full stack')
+})
+
+app.get('/bmi', (request, response) => {
+  const height = Number(request.query.height)
+  const weight = Number(request.query.weight)
+
+  try {
+    const result = calculateBmi(height, weight)
+    response.json({
+      height,
+      weight,
+      bmi: result,
+    })
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      response.status(400).send({
+        error: 'malformatted parameters',
+        message: error.message,
+      })
+    }
+  }
 })
 
 const PORT = 3000
