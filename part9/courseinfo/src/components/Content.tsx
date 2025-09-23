a96fcf83c05ad39f09491490fd663f448a767766
mrhorst <mhorst31@gmail.com>
2025-09-22 21:30:00 -0400

    finish 9.16

--- part9/courseinfo/src/components/Content.tsx
+++ part9/courseinfo/src/components/Content.tsx
@@ -1,20 +1,59 @@
-interface CoursePart {
-  name: string;
-  exerciseCount: number;
-}
+import type { CoursePart, CourseParts } from '../types/CoursePart';
 
-interface CourseParts {
-  courseParts: CoursePart[];
-}
+const Part = ({ part }: { part: CoursePart }) => {
+  switch (part.kind) {
+    case 'basic':
+      return (
+        <div>
+          <h4>
+            {part.name} {part.exerciseCount}
+          </h4>
+          <p>{part.description}</p>
+        </div>
+      );
+    case 'group':
+      return (
+        <div>
+          <h4>
+            {part.name} {part.exerciseCount}
+          </h4>
+          <p>project exercises {part.groupProjectCount}</p>
+        </div>
+      );
+    case 'background':
+      return (
+        <div>
+          <h4>
+            {part.name} {part.exerciseCount}
+          </h4>
+          <p>{part.description}</p>
+          <p>submit to {part.backgroundMaterial}</p>
+        </div>
+      );
+    case 'special':
+      return (
+        <div>
+          <h4>
+            {part.name} {part.exerciseCount}
+          </h4>
+          <p>{part.description}</p>
+          required skills:{' '}
+          {part.requirements.map((req) => (
+            <p key={req}>{req}</p>
+          ))}
+        </div>
+      );
+  }
+};
 
 const Content = ({ courseParts }: CourseParts) => {
   return (
     <div>
       {courseParts.map((part: CoursePart) => {
         return (
-          <p>
-            {part.name} {part.exerciseCount}
-          </p>
+          <div key={part.name}>
+            <Part part={part} />
+          </div>
         );
       })}
     </div>
