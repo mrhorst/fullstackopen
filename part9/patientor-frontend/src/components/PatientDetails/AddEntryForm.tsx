5ffb345d9cac1960c8a7f5c56edcfca2862d4632
mrhorst <mhorst31@gmail.com>
2025-09-30 15:26:31 -0400

    finish 9.29, all entry types. this exercise was quite laborious - should probably refactor state handling at some point...

--- part9/patientor-frontend/src/components/PatientDetails/AddEntryForm.tsx
+++ part9/patientor-frontend/src/components/PatientDetails/AddEntryForm.tsx
@@ -1,10 +1,17 @@
-import { Dispatch, SetStateAction, SyntheticEvent, useState } from 'react';
+import {
+  ChangeEvent,
+  Dispatch,
+  SetStateAction,
+  SyntheticEvent,
+  useState,
+} from 'react';
 import patientService from '../../services/patients';
 import {
   Diagnosis,
   Entry,
-  HealthCheckEntryForm,
+  EntryWithoutId,
   HealthCheckRating,
+  SickLeave,
 } from '../../types';
 import { AxiosError } from 'axios';
 
@@ -31,21 +38,74 @@ export const AddEntryForm = ({
   const [diagnosisCodesArray, setDiagnosisCodesArray] = useState<
     Diagnosis['code'][]
   >([]);
+  const [option, setOption] = useState('HealthCheck');
+  const [dischargeCriteria, setDischargeCriteria] = useState('');
+  const [dischargeDate, setDischargeDate] = useState('');
+  const [employerName, setEmployerName] = useState('');
+  const [sickLeave, setSickLeave] = useState<SickLeave | undefined>(undefined);
+
+  const entryTypes: string[] = [
+    'HealthCheck',
+    'OccupationalHealthcare',
+    'Hospital',
+  ];
 
   const submitForm = async (e: SyntheticEvent) => {
+    e.preventDefault();
     try {
-      e.preventDefault();
-      const entry: HealthCheckEntryForm = {
-        type: 'HealthCheck',
-        description,
-        date,
-        specialist,
-        healthCheckRating,
-        diagnosisCodes: diagnosisCodesArray,
-      };
-      const newEntry = await patientService.addEntry(userId, entry);
+      let payload: EntryWithoutId;
+
+      switch (option) {
+        case 'HealthCheck':
+          payload = {
+            type: 'HealthCheck',
+            description,
+            date,
+            specialist,
+            healthCheckRating,
+            diagnosisCodes: diagnosisCodesArray,
+          };
+          break;
+        case 'OccupationalHealthcare':
+          const hasSickLeave =
+            sickLeave &&
+            (sickLeave?.startDate.trim() || sickLeave?.endDate.trim());
+          payload = {
+            type: 'OccupationalHealthcare',
+            description,
+            date,
+            specialist,
+            employerName,
+            ...(hasSickLeave
+              ? {
+                  sickLeave: {
+                    startDate: sickLeave!.startDate,
+                    endDate: sickLeave!.endDate,
+                  },
+                }
+              : {}),
+            diagnosisCodes: diagnosisCodesArray,
+          };
+          break;
+        case 'Hospital':
+          payload = {
+            type: 'Hospital',
+            description,
+            date,
+            specialist,
+            discharge: { date: dischargeDate, criteria: dischargeCriteria },
+            diagnosisCodes: diagnosisCodesArray,
+          };
+          break;
+        default:
+          throw new Error('incorrect entry type');
+      }
+
+      const newEntry = await patientService.addEntry(userId, payload);
       // optimistic update on patient entries...
       setPatientEntries((prev) => prev.concat(newEntry));
+      setMessage('Entry added successfully');
+      setTimeout(() => setMessage(null), 3000);
       setOpenForm(false);
     } catch (error: unknown) {
       if (error instanceof AxiosError) {
@@ -64,7 +124,7 @@ export const AddEntryForm = ({
     }
   };
 
-  const addDiagnosisCode = (e: SyntheticEvent) => {
+  const addDiagnosisCode = (e: SyntheticEvent): void => {
     e.preventDefault();
     setDiagnosisCodesArray(diagnosisCodesArray.concat(diagnosisCodesInput));
     setDiagnosisCodesInput('');
@@ -86,83 +146,47 @@ export const AddEntryForm = ({
       <h4>New Entry</h4>
       <form onSubmit={submitForm}>
         <div>
-          <div>
-            <label htmlFor='description'>description</label>
-          </div>
-          <input
-            name='description'
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
+          <RadioButtonInput
+            entryTypes={entryTypes}
+            setOption={setOption}
+            option={option}
           />
         </div>
-        <div>
-          <div>
-            <label htmlFor='date'>date</label>
-          </div>
-          <input
-            name='date'
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
+        <BaseEntryInput
+          description={description}
+          setDescription={setDescription}
+          date={date}
+          setDate={setDate}
+          specialist={specialist}
+          setSpecialist={setSpecialist}
+          diagnosisCodesInput={diagnosisCodesInput}
+          setDiagnosisCodesInput={setDiagnosisCodesInput}
+          addDiagnosisCode={addDiagnosisCode}
+          diagnosisCodesArray={diagnosisCodesArray}
+        />
+        {option === 'HealthCheck' && (
+          <HealthCheckInput
+            healthCheckRating={healthCheckRating}
+            setHealthCheckRating={setHealthCheckRating}
           />
-        </div>
-        <div>
-          <div>
-            <label htmlFor='specialist'>specialist</label>
-          </div>
-          <input
-            name='specialist'
-            value={specialist}
-            onChange={(e) => setSpecialist(e.target.value)}
+        )}
+        {option === 'Hospital' && (
+          <HospitalInput
+            setDischargeDate={setDischargeDate}
+            dischargeDate={dischargeDate}
+            setDischargeCriteria={setDischargeCriteria}
+            dischargeCriteria={dischargeCriteria}
           />
-        </div>
-        <div>
-          <div>
-            <label htmlFor='healthCheckRating'>healthCheckRating</label>
-          </div>
-          <input
-            name='healthCheckRating'
-            value={healthCheckRating}
-            onChange={(e) => setHealthCheckRating(Number(e.target.value))}
+        )}
+        {option === 'OccupationalHealthcare' && (
+          <OccupationalHealthcare
+            employerName={employerName}
+            setEmployerName={setEmployerName}
+            sickLeave={sickLeave}
+            setSickLeave={setSickLeave}
           />
-        </div>
-        <div>
-          <div>
-            <label htmlFor='diagnosisCodes'>diagnosis codes</label>
-          </div>
-          <input
-            name='diagnosisCodes'
-            value={diagnosisCodesInput}
-            onChange={(e) => setDiagnosisCodesInput(e.target.value)}
-          />
-          <button onClick={addDiagnosisCode}>add diagnosis code</button>
-        </div>
-        <div>
-          <ul
-            style={{
-              listStyle: 'none',
-              display: 'flex',
-              gap: '5px',
-              margin: '5px 0',
-              padding: 0,
-            }}
-          >
-            {diagnosisCodesArray.map((code) => {
-              return (
-                <li
-                  style={{
-                    border: '1px solid #CCC',
-                    padding: '8px',
-                    backgroundColor: '#999',
-                    borderRadius: '4px',
-                  }}
-                  key={code}
-                >
-                  {code}
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+        )}
+
         <div
           style={{
             display: 'flex',
@@ -174,6 +198,253 @@ export const AddEntryForm = ({
           <button type='submit'>add</button>
         </div>
       </form>
+    </div>
+  );
+};
+
+interface OccupationalHealthcareInputProps {
+  setEmployerName: Dispatch<SetStateAction<string>>;
+  employerName: string;
+  sickLeave?: SickLeave;
+  setSickLeave: Dispatch<SetStateAction<SickLeave | undefined>>;
+}
+
+const OccupationalHealthcare = ({
+  employerName,
+  setEmployerName,
+  sickLeave,
+  setSickLeave,
+}: OccupationalHealthcareInputProps) => {
+  return (
+    <div>
+      <h3>Occupational Healthcare</h3>
+      <div>
+        <label htmlFor='employerName'>employer name</label>
+      </div>
+      <input
+        name='employerName'
+        value={employerName}
+        onChange={(e) => setEmployerName(e.target.value)}
+      />
+      <div>
+        <p>Sick Leave</p>
+        <div>
+          <label htmlFor='sickStart'>start date</label>
+        </div>
+        <input
+          name='sickStart'
+          value={sickLeave?.startDate ?? ''}
+          onChange={(e) =>
+            setSickLeave((prev) => ({
+              ...(prev ?? { startDate: '', endDate: '' }),
+              startDate: e.target.value,
+            }))
+          }
+        />
+        <div>
+          <label htmlFor='sickEnd'>end date</label>
+        </div>
+        <input
+          name='sickEnd'
+          value={sickLeave?.endDate ?? ''}
+          onChange={(e) =>
+            setSickLeave((prev) => ({
+              ...(prev ?? { startDate: '', endDate: '' }),
+              endDate: e.target.value,
+            }))
+          }
+        />
+      </div>
+    </div>
+  );
+};
+
+interface HospitalInputProps {
+  dischargeDate: string;
+  setDischargeDate: Dispatch<SetStateAction<string>>;
+  dischargeCriteria: string;
+  setDischargeCriteria: Dispatch<SetStateAction<string>>;
+}
+
+const HospitalInput = ({
+  dischargeDate,
+  setDischargeDate,
+  dischargeCriteria,
+  setDischargeCriteria,
+}: HospitalInputProps) => {
+  return (
+    <div>
+      <h3>Discharge</h3>
+      <div>
+        <label htmlFor='dischargeDate'>date</label>
+      </div>
+      <input
+        name='dischargeDate'
+        value={dischargeDate}
+        onChange={(e) => setDischargeDate(e.target.value)}
+      />
+      <div>
+        <label htmlFor='dischargeCriteria'>Criteria</label>
+      </div>
+      <input
+        name='dischargeCriteria'
+        value={dischargeCriteria}
+        onChange={(e) => setDischargeCriteria(e.target.value)}
+      />
+    </div>
+  );
+};
+
+interface HealthCheckProps {
+  healthCheckRating: HealthCheckRating;
+  setHealthCheckRating: Dispatch<SetStateAction<HealthCheckRating>>;
+}
+
+const HealthCheckInput = ({
+  healthCheckRating,
+  setHealthCheckRating,
+}: HealthCheckProps) => {
+  return (
+    <div>
+      <div>
+        <label htmlFor='healthCheckRating'>healthCheckRating</label>
+      </div>
+      <input
+        name='healthCheckRating'
+        value={healthCheckRating}
+        onChange={(e) => setHealthCheckRating(Number(e.target.value))}
+      />
+    </div>
+  );
+};
+
+interface BaseInputProps {
+  description: string;
+  setDescription: Dispatch<SetStateAction<string>>;
+  date: string;
+  setDate: Dispatch<SetStateAction<string>>;
+  specialist: string;
+  setSpecialist: Dispatch<SetStateAction<string>>;
+  diagnosisCodesInput: string;
+  setDiagnosisCodesInput: Dispatch<SetStateAction<string>>;
+  addDiagnosisCode: (e: SyntheticEvent) => void;
+  diagnosisCodesArray: string[];
+}
+
+const BaseEntryInput = ({
+  description,
+  setDescription,
+  date,
+  setDate,
+  specialist,
+  setSpecialist,
+  diagnosisCodesInput,
+  setDiagnosisCodesInput,
+  addDiagnosisCode,
+  diagnosisCodesArray,
+}: BaseInputProps) => {
+  return (
+    <div>
+      <div>
+        <div>
+          <label htmlFor='description'>description</label>
+        </div>
+        <input
+          name='description'
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+      </div>
+      <div>
+        <div>
+          <label htmlFor='date'>date</label>
+        </div>
+        <input
+          name='date'
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
+      </div>
+      <div>
+        <div>
+          <label htmlFor='specialist'>specialist</label>
+        </div>
+        <input
+          name='specialist'
+          value={specialist}
+          onChange={(e) => setSpecialist(e.target.value)}
+        />
+      </div>
+      <div>
+        <div>
+          <label htmlFor='diagnosisCodes'>diagnosis codes</label>
+        </div>
+        <input
+          name='diagnosisCodes'
+          value={diagnosisCodesInput}
+          onChange={(e) => setDiagnosisCodesInput(e.target.value)}
+        />
+        <button onClick={addDiagnosisCode}>add diagnosis code</button>
+      </div>
+      <div>
+        <ul
+          style={{
+            listStyle: 'none',
+            display: 'flex',
+            gap: '5px',
+            margin: '5px 0',
+            padding: 0,
+          }}
+        >
+          {diagnosisCodesArray.map((code) => {
+            return (
+              <li
+                style={{
+                  border: '1px solid #CCC',
+                  padding: '8px',
+                  backgroundColor: '#999',
+                  borderRadius: '4px',
+                }}
+                key={code}
+              >
+                {code}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RadioButtonInput = ({
+  entryTypes,
+  setOption,
+  option,
+}: {
+  entryTypes: string[];
+  setOption: Dispatch<SetStateAction<string>>;
+  option: string;
+}) => {
+  const handleSelectOption = (e: ChangeEvent<HTMLInputElement>) => {
+    setOption(e.target.value);
+  };
+
+  return (
+    <div style={{ marginBottom: '10px' }}>
+      {entryTypes.map((type) => {
+        return (
+          <div key={type}>
+            <input
+              value={type}
+              type='radio'
+              checked={option === type}
+              onChange={handleSelectOption}
+            />
+            <label>{type}</label>
+          </div>
+        );
+      })}
     </div>
   );
 };
